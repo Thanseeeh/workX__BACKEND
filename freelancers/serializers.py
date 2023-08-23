ed273178191770e7b236bdf379cda53a9dc21adf
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-08-23 07:18:14 +0530

    backend, usermanagement for users

--- freelancers/serializers.py
+++ freelancers/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
+from accounts.models import Account
 from accounts.serializers import UserViewSerializer
 from .models import FreelancerProfile
 
+
+
+class FreelancerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['username', 'first_name', 'last_name', 'email', 'phone_number', 'is_verified', 'is_active']
 
 class FreelancerProfileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,6 +17,8 @@ class FreelancerProfileSerializer(serializers.ModelSerializer):
 
 
 class FreelancerProfileListSerializer(serializers.ModelSerializer):
+    freelancer = FreelancerSerializer()
+
     class Meta:
         model = FreelancerProfile
         fields = '__all__'
--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
+from accounts.models import Account
 from accounts.serializers import UserViewSerializer
 from .models import UserProfile
 
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['username', 'first_name', 'last_name', 'email', 'phone_number', 'is_verified', 'is_active']
+
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        exclude = ('freelancer',)
+        exclude = ('user',)
 
 
 class UserProfileListSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = UserProfile
         fields = '__all__'
